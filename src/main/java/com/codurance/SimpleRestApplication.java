ef16efa19b98cbaa32f84f5c7086ad2ee6c56001
Alex Short <alex.short@digitaslbi.com>
2016-04-24 15:15:17 +0100

    Update SimpleRestApplication.java

--- src/main/java/com/codurance/SimpleRestApplication.java
+++ src/main/java/com/codurance/SimpleRestApplication.java
@@ -7,13 +7,21 @@ import static spark.Spark.get;
 
 public class SimpleRestApplication {
     public static void main(String[] args) {
-        get("/hello", respondWith(new HelloWorld().sayHelloWorld()));
+        get("/hello", respondWithHtml(new HelloWorld().sayHelloWorld()));
         get("/healthcheck", respondWith("OK"));
     }
 
     private static Route respondWith(Object responseBody){
         return (request, response) -> {
             response.type("text/plain");
+            response.status(HttpStatus.OK_200);
+            return responseBody;
+        };
+    }
+    
+        private static Route respondWithHtml(Object responseBody){
+        return (request, response) -> {
+            response.type("text/HTML");
             response.status(HttpStatus.OK_200);
             return responseBody;
         };
