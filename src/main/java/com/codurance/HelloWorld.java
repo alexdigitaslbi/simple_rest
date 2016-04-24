298bc6c3827d680974eec627d087359fbfcfff90
Alex Short <alex.short@digitaslbi.com>
2016-04-24 15:02:12 +0100

    Update HelloWorld.java

--- src/main/java/com/codurance/HelloWorld.java
+++ src/main/java/com/codurance/HelloWorld.java
@@ -3,6 +3,6 @@ package com.codurance;
 public class HelloWorld {
 
     public String sayHelloWorld() {
-        return "Hello World";
+        return "<html><body><img src='https://jefferywilcox.files.wordpress.com/2010/02/spiff81.jpg'></body></html>";
     }
 }
